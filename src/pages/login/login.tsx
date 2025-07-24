15767c473e967ae6d784cf76e716d858a441f7bc
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2025-07-24 16:07:12 +0545

    First component test

---
+++ src/pages/login/login.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const LoginPage = () => {
+  return (
+    <div>
+      <h1>Sign In</h1>
+      <input type="text" placeholder="Username" />
+      <input type="text" placeholder="Password" />
+
+      <button>Log in</button>
+
+      <label htmlFor="remember-me">Remember me</label>
+      <input type="checkbox" id="remember-me" />
+
+      <a href="#">Forgot password</a>
+    </div>
+  );
+};
+
+export default LoginPage;
