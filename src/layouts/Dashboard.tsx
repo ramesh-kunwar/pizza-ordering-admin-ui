0848f75cd6382cdf83302c62f7cc3e5878ddfac2
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2025-07-27 08:32:27 +0545

    Add sidebar layout

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -1,16 +1,86 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, NavLink, Outlet } from "react-router-dom";
 import { useAuthStore } from "../store";
+import { Layout, Menu, theme } from "antd";
+import { Content, Footer, Header } from "antd/es/layout/layout";
+import Sider from "antd/es/layout/Sider";
+import Icon, { UserOutlined } from "@ant-design/icons";
+import { useState } from "react";
+import Logo from "../components/icons/Logo";
+import Home from "../components/icons/Home";
+import { FoodIcon } from "../components/icons/FoodIcon";
+import BasketIcon from "../components/icons/BasketIcon";
+import GiftIcon from "../components/icons/GiftIcon";
+
+const items = [
+  {
+    key: "/",
+    icon: <Icon component={Home} />,
+    label: <NavLink to="/">Home</NavLink>,
+  },
+
+  {
+    key: "/users",
+    icon: <UserOutlined />,
+
+    label: <NavLink to="/users">Users</NavLink>,
+  },
+
+  {
+    key: "/products",
+    icon: <Icon component={FoodIcon} />,
+    label: <NavLink to="/products">Products</NavLink>,
+  },
+  {
+    key: "/orders",
+    icon: <Icon component={BasketIcon} />,
+    label: <NavLink to="/orders">Orders</NavLink>,
+  },
+  {
+    key: "/promos",
+    icon: <Icon component={GiftIcon} />,
+    label: <NavLink to="/promos">Promos</NavLink>,
+  },
+];
 
 const Dashboard = () => {
+  const [collapsed, setcollapsed] = useState(false);
+
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+
   const { user } = useAuthStore();
+
   if (user === null) {
     return <Navigate to="/auth/login" replace={true} />;
   }
   return (
     <div>
-      <h1>Dashboard component</h1>
-
-      <Outlet />
+      <Layout style={{ minHeight: "100vh", background: colorBgContainer }}>
+        <Sider
+          theme="light"
+          collapsible
+          collapsed={collapsed}
+          onCollapse={(value) => setcollapsed(value)}
+        >
+          <div className="logo">
+            <Logo />
+          </div>
+          <Menu
+            theme="light"
+            defaultSelectedKeys={["1"]}
+            mode="inline"
+            items={items}
+          />
+        </Sider>
+        <Layout>
+          <Header style={{ padding: 0, background: colorBgContainer }} />
+          <Content style={{ margin: "0 16px" }}>
+            <Outlet />
+          </Content>
+          <Footer style={{ textAlign: "center" }}>Mernspace pizza shop</Footer>
+        </Layout>
+      </Layout>
     </div>
   );
 };
--- src/router.tsx
+++ src/router.tsx
@@ -5,6 +5,7 @@ import LoginPage from "./pages/login/login";
 import Dashboard from "./layouts/Dashboard";
 import NonAuth from "./layouts/NonAuth";
 import Root from "./layouts/Root";
+import { Users } from "./pages/users/Users";
 
 export const router = createBrowserRouter([
   {
@@ -18,6 +19,11 @@ export const router = createBrowserRouter([
           {
             path: "",
             element: <HomePage />,
+          },
+
+          {
+            path: "/users",
+            element: <Users />,
           },
         ],
       },
